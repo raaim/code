f63d53dca52ab0363670d87f0cc276bba29152d0
raaim <raaim@protonmail.com>
2018-06-15 22:54:11 +0800

    excercise

---
+++ ex.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+    std::cout << "Please enter two floating-point values:\n";
+    double var1;
+    double var2;
+    std::cin >> var1 >> var2;
+        if ( var1 > var2 )
+            std::cout << var1 << " is larger than " << var2 << "\n";
+        if ( var1 < var2 )
+            std::cout << var1 << " is smaller than " << var2 << "\n";
+        if ( var1 == var2 )
+            std::cout << var1 << " is equal to " << var2 << "\n";
+}
