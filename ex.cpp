f2d2fabe6e9d875a71eec33d04a02484ba7b1ee3
raaim <raaim@protonmail.com>
2018-06-15 23:22:08 +0800

    yeet2

--- ex.cpp
+++ ex.cpp
@@ -12,4 +12,5 @@ int main()
             std::cout << var1 << " is smaller than " << var2 << "\n";
         if ( var1 == var2 )
             std::cout << var1 << " is equal to " << var2 << "\n";
+    return 0;
 }
