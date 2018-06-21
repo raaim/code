5baadbc99b4d60dc2033be1078147aae3b9a5f02
raaim <raaim@protonmail.com>
2018-06-21 16:06:25 +0800

    Converter

--- conv.cpp
+++ conv.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 using namespace std;
 
-
 int main()
 {
     constexpr double dollar_to_yen = 110.13;
@@ -13,25 +12,31 @@ int main()
     constexpr double dollar_to_euro = 0.86;
 
     cout << "Please enter the value of your currency followed by a letter for currency.\nY for yen, M for myr and E for euro.\nIf you want to convert from dollars to the three currencies then enter D\n";
+    
     double value;
     char currency = 0;
 
     cin >> value >> currency;
 
-    switch(currency){
+    switch (currency)
+    {
     
     case 'Y':
-        cout << "That is equivalent to " << value/dollar_to_yen << " dollars\n";
+        cout << "That is equivalent to " << value / dollar_to_yen << " dollars\n";
         break;
+    
     case 'M':
-        cout << "That is equivalent to " << value/dollar_to_myr << " dollars\n";
+        cout << "That is equivalent to " << value / dollar_to_myr << " dollars\n";
         break;
+    
     case 'E':
-        cout << "That is equivalent to " << value/dollar_to_euro << " dollars\n";
+        cout << "That is equivalent to " << value / dollar_to_euro << " dollars\n";
         break;
+    
     default:
         cout << "I don't know that currency";
         break;
+    
     }
 
     return 0;
