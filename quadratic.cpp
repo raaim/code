0293670a6c29db5bf28dc56af0ad8c5f1db5e128
raaim <raaim@protonmail.com>
2018-07-20 19:28:20 +0800

    update 2

--- quadratic.cpp
+++ quadratic.cpp
@@ -14,6 +14,7 @@ int main()
 
     double root1 = (-b+sqrt((b*b)-4*a*c))/(2*a); //Quadratic formula (-b±√b²-4ac)/(2a)
     double root2 = (-b-sqrt((b*b)-4*a*c))/(2*a);
+    
     if (((b*b) - 4*a*c) < 0)                    //Test if real roots are present. if b²-4ac is less than zero
         cout << "Function has no real roots\n"; //then the function has no real roots.
     else 
