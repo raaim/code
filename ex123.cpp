9af14a0dc9bb390073e81507b1fbdd7faef97692
raaim <raaim@protonmail.com>
2018-07-20 19:07:23 +0800

    UPDATE 2

--- ex123.cpp
+++ ex123.cpp
@@ -12,15 +12,11 @@ double ctok(double c)
     return k;
 }
 
+
 int main()
 {
     double c = 0; // declare input variable
-    cin >> c;
-    if(c > -273.15){                 // retrieve temperature to input variable
-        double k = ctok(c);         // convert temperature
-        cout << k << '\n' ;          // print out temperature
-    }
-    else{
-        cerr << "Temperature cannot be below absolute zero (-273.15°C)\n";
-    }
+    cin >> c;                // retrieve temperature to input variable
+    double k = ctok(c);         // convert temperature
+    cout << k << " k\n" ;          // print out temperature
 }
